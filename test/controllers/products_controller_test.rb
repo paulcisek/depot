5e8b60d8ece274bb853d96490e90df32d5fcda49
paulcisek <paul.cisek@outlook.com>
2014-09-15 17:35:14 +0200

    Task C: Catalog display

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,14 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'h1', 'Listing products'
+    assert_select 'table tr', 3
+    assert_select 'a', 'New product'
+    assert_select '.list_actions' do |elements|
+      elements.each do |element|
+        assert_select element, "a", 3
+      end
+    end
   end
 
   test "should get new" do
