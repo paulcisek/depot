36d47af850b71ab91cba9c4f44ae79bce7756aef
paulcisek <paul.cisek@outlook.com>
2014-09-15 18:54:26 +0200

    Task D: Cart Creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,11 @@
 class StoreController < ApplicationController
+  before_action :increment_store_view_counter, only: [:index]
   def index
  	@products = Product.order(:title)
   end
+
+  private
+  	def increment_store_view_counter
+  		@counter = session[:counter].nil? ? session[:counter] = 0 : session[:counter] += 1
+  	end
 end
